0b66422d05385a4cf9ea91c8c8ab345078931816
MrL <1729713807@qq.com>
2018-10-22 09:56:45 +0800

    sign_in

--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -5,66 +5,66 @@
     <router-link to="/">
       <b-navbar-brand>
         <b-img width="85" height="50"
-               src="https://cdn2.jianshu.io/assets/web/nav-logo-4c7bbafe27adc892f3046e6978459bac.png"/>
+               src="../../static/img/logo.png"/>
       </b-navbar-brand>
     </router-link>
 
     <b-collapse is-nav id="nav_collapse">
-      <b-navbar-nav style="margin-left: 85px">
-        <b-nav-item style="padding-right: 35px" v-if="token!=null">
-          <router-link to="/index">
-            <b-img src="/static/img/find.png"/>
-            发现
-          </router-link>
+      <b-navbar-nav>
+        <b-nav-item v-if="user!=null">
+          <router-link to="/">发现</router-link>
         </b-nav-item>
-        <b-nav-item style="padding-right: 35px" v-if="token!=null">
+        <b-nav-item v-if="user!=null">
           <router-link to="/subscriptions">关注</router-link>
         </b-nav-item>
-        <b-nav-item-hover style="padding-right: 35px" v-if="token!=null">
-          <b-nav-item-dropdown right>
-            <template slot="button-content">
-             消息
-            </template>
-            <b-dropdown-item>个人资料</b-dropdown-item>
-            <b-dropdown-item>退出</b-dropdown-item>
-          </b-nav-item-dropdown>
-      </b-nav-item-hover>
+        <b-nav-item v-if="user!=null">
+          <router-link to="/notifications">消息</router-link>
+        </b-nav-item>
 
-      <b-nav-item style="padding-right: 35px" v-if="token===null">
-        <router-link to="/index">首页</router-link>
-      </b-nav-item>
-      <b-nav-item style="padding-right: 35px" v-if="token===null">
-        <router-link to="/">下载</router-link>
-      </b-nav-item>
+        <b-nav-item v-if="user===null">
+          <router-link to="/index">首页</router-link>
+        </b-nav-item>
+
+        <b-nav-item v-if="user===null">
+          <router-link to="/download">下载APP</router-link>
+        </b-nav-item>
+
+        <b-nav-form>
+          <b-form-input size="sm" class="mr-sm-2 search-input" type="text" placeholder="Search"/>
+          <font-awesome-icon icon="search" style="color: #999;font-size: 20px;margin-left: -50px;"/>
+        </b-nav-form>
       </b-navbar-nav>
-
-      <b-nav-form>
-        <b-form-input size="sm" class="mr-sm-2" type="text" placeholder="Search"/>
-      </b-nav-form>
 
       <!-- Right aligned nav items -->
       <b-navbar-nav class="ml-auto">
         <b-nav-item href="#">Aa</b-nav-item>
-        <b-nav-item v-if="token===null">
-          <router-link to="/sign-in">登录</router-link>
+        <b-nav-item v-if="user===null">
+          <router-link to="/sign_in">登录</router-link>
         </b-nav-item>
-        <b-nav-item v-if="token===null">
-          <router-link to="/sign-up">注册</router-link>
+
+        <b-nav-item v-if="user===null">
+          <router-link to="/sign_up">注册</router-link>
         </b-nav-item>
-        <b-nav-item-dropdown right v-if="token!=null">
+        <b-nav-item-dropdown right v-if="user!=null">
           <!-- Using button-content slot -->
           <b-dropdown-item>
             <router-link to="/u">
               个人中心
             </router-link>
           </b-dropdown-item>
+          <b-dropdown-item>
+            <router-link to="/setting">
+              设置
+            </router-link>
+          </b-dropdown-item>
           <template slot="button-content">
-            <b-img rounded="circle" width="35" height="35" src="../../static/img/101.jpg"/>
+            <b-img rounded="circle" width="35" height="35" :src="user.avatar"/>
           </template>
-          <b-dropdown-item href="#">设置</b-dropdown-item>
-          <b-dropdown-item href="#">退出</b-dropdown-item>
+          <b-dropdown-item @click="logout">
+            退出
+          </b-dropdown-item>
         </b-nav-item-dropdown>
-        <a class="btn write-btn" href="/write" v-if="token!=null">
+        <a class="btn write-btn" href="/write">
           写文章
         </a>
       </b-navbar-nav>
@@ -73,8 +73,6 @@
   </b-navbar>
 </template>
 <script>
-
-
   import 'bootstrap/dist/css/bootstrap.css'
   import 'jquery/dist/jquery.min'
   import 'bootstrap/dist/js/bootstrap.min'
@@ -82,30 +80,18 @@
   export default {
     data() {
       return {
-        name: 'cctN',
-        token: '11231'
-      }
-    },
-    computed: {
-      username() {
-        let username = localStorage.getItem('ms_username');
-        return username ? username : this.name;
+        user: JSON.parse(localStorage.getItem('loginUser'))
       }
     },
     methods: {
-      // 用户名下拉菜单选择事件
-      handleCommand(command) {
-        if (command == 'loginout') {
-          localStorage.removeItem('ms_username')
-          this.$router.push('/login');
-        }
+      logout() {
+        localStorage.removeItem('loginUser')
+        this.$router.go(0);
       }
-
     }
   }
 </script>
 <style scoped>
-
   .box {
     border-bottom: 1px solid #eef1f6;
     height: 55px;
@@ -113,10 +99,6 @@
 
   a {
     color: #324157;
-  }
-
-  .btn {
-
   }
 
   .write-btn {
@@ -131,5 +113,13 @@
     background-color: #ea6f5a;
   }
 
-
+  .search-input {
+    padding: 0 40px 0 20px;
+    width: 160px;
+    height: 38px;
+    font-size: 14px;
+    border: 1px solid #eee;
+    border-radius: 40px;
+    background: #eee;
+  }
 </style>
